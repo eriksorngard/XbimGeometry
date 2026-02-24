a1cf93774f7284c0daa9686874670f58491e651a
Erik Sørngård <erik.sorngard@gl-instrumenter.no>
2026-02-24 18:29:47 +0100

    Get list of cartesian points in curves not part of polylines.

--- Xbim.Geometry.Engine.Tests/DigPilotTests.cs
+++ Xbim.Geometry.Engine.Tests/DigPilotTests.cs
@@ -18,21 +18,22 @@ public class DigPilotTests
     }
 
     [Theory]
-    [InlineData("Stømner.ifc", 3)]
+    ////[InlineData("Stømner.ifc", 3)]
     ////[InlineData("StikkrenneTestAH.ifc", 1)]
-    ////[InlineData("C_OMS_FM_GEN_DRE_1100_Sporv prosjektert anlegg.ifc", 23)]
+    [InlineData("C_OMS_FM_GEN_DRE_1100_Sporv prosjektert anlegg.ifc", 23, 6)]
     ////[InlineData("20200270_Bygg21_LARK.ifc", 0)]
     ////[InlineData("Example files for IFC export\\Ifx4x3-case6-mapcoordinates.ifc", 3)]
-    public void GetCurves(string fileName, int expectedCount)
+    public void GetCurvesAndPoints_FromModel_CountsAreAsExpected(string fileName, int expectedCurveCount, int expectedPointCount)
     {
         var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
         using var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 1024 * 4, FileOptions.Asynchronous | FileOptions.SequentialScan);
         using var model = IfcStore.Open(stream, IO.StorageType.Ifc, IO.XbimModelType.MemoryModel);
         var context = new Xbim3DModelContext(model, _loggerFactory, XGeometryEngineVersion.V6);
 
-        var result = context.CreateContext(generateCurves: true);
+        var result = context.CreateContext(generateCurvesAndPoints: true);
 
         result.Should().Be(true);
-        context.Curves.Should().HaveCount(expectedCount);
+        context.Curves.Should().HaveCount(expectedCurveCount);
+        context.Points.Should().HaveCount(expectedPointCount);
     }
 }
